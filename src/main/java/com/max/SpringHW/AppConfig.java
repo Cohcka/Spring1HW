687c3736994e92b91fb90fd75487fcccd950f24a
MaxCohcka <Cohcka@gmail.com>
2022-08-18 21:44:12 +0300

    Start HW3

--- src/main/java/com/max/SpringHW/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.max.SpringHW;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.max.SpringHW")
-public class AppConfig {
-}
---
+++ src/test/java/com/example/spring1HW3/Spring1Hw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring1HW3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1Hw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
